4172518705896570490ca78afa7282882e497596
Christian <cw94@live.se>
2022-10-26 11:04:28 +0200

    init

--- Movie.cs
+++ Movie.cs
@@ -10,3 +10,14 @@ RetireMovie(Movie)
 ChangeStockAmount(Movie)
 RemoveMovie(Movie)
 CheckForLateReturns(Movie) */
+public class Movie
+{
+    public int ID { get; set; }
+    public int Title_ID { get; set; }
+    public int Customer_ID { get; set; }
+    public DateTime Store_Purchase_Date { get; set; }
+    public bool Is_Retired { get; set; }
+    public float Rental_Price { get; set; }
+    public DateTime Rent_Date { get; set; }
+    public DateTime Return_Date { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿using Dapper;
 using MySqlConnector;
+using static System.Console;
+
+using (var connection = new MySqlConnection("Server=localhost;Database=videoteket;Uid=krister;Pwd=apa123;"))
+{
+    var movie = connection.Query<Movie>("SELECT ID, Title_ID, Customer_ID, Store_Purchase_Date, Is_Retired, Rental_Price, Rent_Date, Return_Date FROM movie;").ToList();
+    foreach (Movie m in movie)
+    {
+        WriteLine(m.ID + "  " + m.Title_ID + " " + m.Customer_ID + " " + m.Store_Purchase_Date + " " + m.Is_Retired + " " + m.Rental_Price + " " + m.Rent_Date + " " + m.Return_Date);
+    }
+}
+ReadLine();
