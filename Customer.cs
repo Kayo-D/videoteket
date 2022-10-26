0d7a84e0eebefc097cb431b3509a4f912e69c435
Christian <cw94@live.se>
2022-10-26 12:45:06 +0200

    progress

--- Customer.cs
+++ Customer.cs
@@ -1,9 +1,6 @@
 using Dapper;
 using MySqlConnector;
-using (var connection = new MySqlConnection("Server=localhost;Database=videoteket;Uid=root;"))
-{
-    var customerList = connection.Query<Customer>("SELECT ID, Name, Address, Social_Security_Number, Phone_Number, Email FROM customer;").ToList();
-}
+using (var connection = new MySqlConnection("Server=localhost;Database=videoteket;Uid=root;"));
 public class Customer
 {
     public int ID { get; set; }
@@ -12,6 +9,7 @@ public class Customer
     public string Social_Security_Number { get; set; }
     public string Phone_Number { get; set; }
     public string Email { get; set; }
+    private var customerList = connection.Query<Customer>("SELECT ID, Name, Address, Social_Security_Number, Phone_Number, Email FROM customer;").ToList();
     public void CreateCustomer()
     {
         
