745e9b2e0ab75585bdf5bcfd4d1d489e14b025e8
Christian <cw94@live.se>
2022-10-26 13:08:40 +0200

    init

--- Customer.cs
+++ Customer.cs
@@ -1,6 +1,5 @@
 using Dapper;
 using MySqlConnector;
-using (var connection = new MySqlConnection("Server=localhost;Database=videoteket;Uid=root;"));
 public class Customer
 {
     public int ID { get; set; }
@@ -9,7 +8,6 @@ public class Customer
     public string Social_Security_Number { get; set; }
     public string Phone_Number { get; set; }
     public string Email { get; set; }
-    private List<Customer> customerList = connection.Query<Customer>("SELECT ID, Name, Address, Social_Security_Number, Phone_Number, Email FROM customer;").ToList();
     public void CreateCustomer()
     {
         
