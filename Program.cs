21df2b103885f192367b3b9caf4aef424bf078e8
Christian <cw94@live.se>
2022-10-26 11:14:47 +0200

    init

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using MySqlConnector;
 using static System.Console;
 
-using (var connection = new MySqlConnection("Server=localhost;Database=videoteket;Uid=krister;Pwd=apa123;"))
+using (var connection = new MySqlConnection("Server=localhost;Database=videoteket;Uid=root;"))
 {
     var movie = connection.Query<Movie>("SELECT ID, Title_ID, Customer_ID, Store_Purchase_Date, Is_Retired, Rental_Price, Rent_Date, Return_Date FROM movie;").ToList();
     foreach (Movie m in movie)
